49d155c3dc2bc744f36f0877f85883ff9fa92243
odeyseed167-creator <odeyseed167@gmail.com>
2025-09-27 20:13:01 +0300

    Start the actual crawl and await the result.

--- lib/linkcheck.dart
+++ lib/linkcheck.dart
@@ -231,4 +231,98 @@ Future<int> run(List<String> arguments, Stdout stdout) async {
   var urls = argResults.rest.toList();
   var skipper = UrlSkipper.empty();
 
-  
+  if (inputFile != null) {
+    final file = File(inputFile);
+    try {
+      urls.addAll(file.readAsLinesSync().where((url) => url.isNotEmpty));
+    } on FileSystemException catch (e) {
+      print("Can't read input file '$inputFile': $e");
+      return 2;
+    }
+  }
+
+  if (skipFile != null) {
+    final file = File(skipFile);
+    try {
+      skipper = UrlSkipper(file.path, file.readAsLinesSync());
+    } on FileSystemException catch (e) {
+      print("Can't read skip file '$skipFile': $e");
+      return 2;
+    }
+  }
+
+  urls = urls.map(_sanitizeSeedUrl).toList();
+
+  if (urls.isEmpty) {
+    print('No URL given, checking $defaultUrl');
+    urls.add(defaultUrl);
+  } else if (verbose) {
+    print('Reading URLs:');
+    urls.forEach(print);
+  }
+
+  // TODO: exit gracefully if provided URL isn't a parseable URI
+  final uris = urls.map((url) => Uri.parse(url)).toList(growable: false);
+  Set<String> hosts;
+  if ((argResults[hostsFlag] as Iterable<String>).isNotEmpty) {
+    hosts = Set<String>.from(argResults[hostsFlag] as Iterable<String>);
+  } else {
+    // No host globs provided. Using the default (http://example.com/**).
+    hosts = uris.map((uri) {
+      var url = uri.toString();
+      if (uri.path.isEmpty) return '$url/**';
+      if (uri.path == '/') return '$url**';
+      if (url.endsWith('/')) url = url.substring(0, url.length - 1);
+      return '$url**';
+    }).toSet();
+  }
+
+  // Start the actual crawl and await the result.
+  final result = await crawl(uris, hosts, shouldCheckExternal, skipper, verbose,
+      ansiTerm, ProcessSignal.sigint.watch(), stdout);
+
+  final broken = result.destinations
+      .where((destination) =>
+          destination.wasTried &&
+          destination.isBroken &&
+          (!reportConnectionFailuresAsWarnings || !destination.didNotConnect))
+      .length;
+
+  final withWarning = result.links
+      .where((link) =>
+          link.hasWarning(shouldCheckAnchors) ||
+          reportConnectionFailuresAsWarnings && link.destination.didNotConnect)
+      .length;
+
+  final withInfo = result.links.where((link) => link.hasInfo).length;
+
+  final withRedirects =
+      result.links.where((link) => link.destination.isRedirected).length;
+
+  if (broken == 0 &&
+      withWarning == 0 &&
+      withInfo == 0 &&
+      (!showRedirects || withRedirects == 0)) {
+    printStats(result, broken, withWarning, withInfo, withRedirects,
+        showRedirects, ansiTerm, stdout);
+  } else {
+    if (ansiTerm) {
+      Console.write('\r');
+      Console.eraseLine(3);
+      print('Done crawling.                   ');
+    }
+
+    reportForWriters(
+        result, ansiTerm, shouldCheckAnchors, showRedirects, stdout);
+
+    printStats(result, broken, withWarning, withInfo, withRedirects,
+        showRedirects, ansiTerm, stdout);
+  }
+  print('');
+
+  if (broken > 0) return 2;
+  if (withWarning > 0 || (showRedirects && withRedirects > 0)) return 1;
+  return 0;
+}
+
+
