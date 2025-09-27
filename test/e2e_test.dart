5988f6d7389c3c75e603e67e2e6a7facceffc611
odeyseed167-creator <odeyseed167@gmail.com>
2025-09-28 00:17:22 +0300

    import linkcheck

--- test/e2e_test.dart
+++ test/e2e_test.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:dhttpd/dhttpd.dart';
-//import 'package:linkcheck/linkcheck.dart' show run;
+import 'package:linkcheck/linkcheck.dart' show run;
 //import 'package:path/path.dart' as path;
 //import 'package:test/test.dart';
 
