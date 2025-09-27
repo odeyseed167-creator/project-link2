ce5a0b39574d2015d422ca38daf67ff9e226588d
odeyseed167-creator <odeyseed167@gmail.com>
2025-09-27 20:31:27 +0300

    this is linkcheck.dart

---
+++ bin/linkcheck.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:linkcheck/linkcheck.dart';
+
+Future<int> main(List<String> arguments) async {
+  await runZonedGuarded(() async {
+    // Run the link checker. The returned value will be the program's exit code.
+    exitCode = await run(arguments, stdout);
+  }, (Object e, StackTrace stackTrace) {
+    stderr.writeln('INTERNAL ERROR: Sorry! Please open '
+        'https://github.com/filiph/linkcheck/issues/new '
+        'in your favorite browser and copy paste the following output there:'
+        '\n');
+    stderr.writeln(e.toString());
+    stderr.writeln(stackTrace.toString());
+    exitCode = 2;
+  });
+  return exitCode;
+}
