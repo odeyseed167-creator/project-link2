749cc9ce6ba633674add1bbf3da1f2fc7f03b4d0
odeyseed167-creator <odeyseed167@gmail.com>
2025-09-27 21:02:50 +0300

    glob_test.dart void main

--- test/glob_test.dart
+++ test/glob_test.dart
@@ -3,3 +3,32 @@ import 'package:test/test.dart';
 
 import 'dart:async';
 import 'package:linkcheck/src/worker/fetch_options.dart';
+
+void main() {
+  test('parses simple example', () {
+    final sink = StreamController<WorkerTask>();
+    final options = FetchOptions(sink);
+    final uri = Uri.parse('http://localhost:4000/');
+    options.addHostGlobs(['$uri**']);
+    expect(options.matchesAsInternal(uri), isTrue);
+    unawaited(sink.close());
+  });
+
+  test('parses localhost:4000/guides', () {
+    final sink = StreamController<WorkerTask>();
+    final options = FetchOptions(sink);
+    final uri = Uri.parse('http://localhost:4000/guides');
+    options.addHostGlobs(['$uri**']);
+    expect(options.matchesAsInternal(uri), isTrue);
+    unawaited(sink.close());
+  });
+
+  test('parses localhost:4000/guides/', () {
+    final sink = StreamController<WorkerTask>();
+    final options = FetchOptions(sink);
+    final uri = Uri.parse('http://localhost:4000/guides/');
+    options.addHostGlobs(['http://localhost:4000/guides**']);
+    expect(options.matchesAsInternal(uri), isTrue);
+    unawaited(sink.close());
+  });
+}
