243582e3ae44801ba73676e8fffcfb85a84aa298
odeyseed167-creator <odeyseed167@gmail.com>
2025-09-27 20:00:37 +0300

    this is my const

--- lib/linkcheck.dart
+++ lib/linkcheck.dart
@@ -10,3 +10,26 @@ import 'src/crawl.dart' show CrawlResult, crawl;
 import 'src/parsers/url_skipper.dart';
 import 'src/writer_report.dart' show reportForWriters;
 
+
+export 'src/crawl.dart' show CrawlResult, crawl;
+export 'src/destination.dart' show Destination;
+export 'src/link.dart' show Link;
+export 'src/origin.dart' show Origin;
+export 'src/writer_report.dart' show reportForWriters;
+
+const anchorFlag = 'check-anchors';
+const ansiFlag = 'nice';
+const connectionFailuresAsWarnings = 'connection-failures-as-warnings';
+const debugFlag = 'debug';
+const defaultUrl = 'http://localhost:8080/';
+const externalFlag = 'external';
+const helpFlag = 'help';
+const hostsFlag = 'hosts';
+const inputFlag = 'input-file';
+const redirectFlag = 'show-redirects';
+const skipFlag = 'skip-file';
+const version = '3.1.0';
+const versionFlag = 'version';
+final _portOnlyRegExp = RegExp(r'^:\d+$');
+
+
